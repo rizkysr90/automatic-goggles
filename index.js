9c38c1693be2202744dce0a050c62b8d071776e6
Rizki Susilo Ramadhan <rizkysr90@gmail.com>
2022-10-01 09:17:12 +0700

    init express

---
+++ index.js
@@ -0,0 +1,8 @@
+import app from './src/app.js';
+
+const port = 3030;
+app.listen(port,function() {
+    console.log('Server listening at port', port);
+})
+
+
---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+
+app.get('/',function(req,res) {
+    // 
+})
+
+
+export default app;
